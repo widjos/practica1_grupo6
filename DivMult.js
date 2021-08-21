cc600518d50269d128ee13b45ccd8f3b3fc09695
widjos <widjos@users.noreply.github.com>
2021-08-21 01:28:22 -0600

    Division entre cero validada - 201602952

--- DivMult.js
+++ DivMult.js
@@ -4,5 +4,5 @@ function multiplicar(m1,m2){
 }
 
 function dividir(m1,m2){
-   return m2 != 0 ? m1/m2 : console.log("La division entre 0 no es valida");
+   return m2 != 0 ? m1/m2 : console.log("Division entre 0 no valida");
 }
